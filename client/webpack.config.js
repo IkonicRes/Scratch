62c0ea8883114358892949e4b081b91a8c68d298
IkonicResonance <67353747+IkonicResonance@users.noreply.github.com>
2023-09-17 07:40:30 -0500

    manifest generating

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
+const ASSET_PATH = process.env.ASSET_PATH || "/"
 module.exports = () => {
   return {
     mode: 'development',
@@ -15,17 +16,21 @@ module.exports = () => {
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: 'images/[hash][ext][query]',
+      publicPath: ASSET_PATH,  
     },
     plugins: [
         new HtmlWebpackPlugin({
           template: './index.html',
-          title: 'Webpack Plugin'
+          title: 'Webpack Plugin',
+          manifest: './manifest.0af4fa0c920d844004154050a48b575e.json'
         }),
         new MiniCssExtractPlugin(),
-        new InjectManifest({
-          swSrc: '/src-sw.js',
-          swDest: 'service-worker.js',
+        new WorkboxWebpackPlugin.InjectManifest({
+          swSrc: "/src-sw.js",
+          swDest: "service-worker.js"
         }),
+        // new FaviconsWebpackPlugin('./src/images/logo.png'),
         new WebpackPwaManifest({
           name: 'Scratch',
           short_name: 'Scratch',
@@ -35,8 +40,9 @@ module.exports = () => {
           start_url: '/',
           icons: [
             {
-              src: path.resolve('src/icon.png'),
+              src: path.resolve('src/images/logo.png'),
               sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons')
             },
           ],
         }),
@@ -47,11 +53,10 @@ module.exports = () => {
           test: /\.css$/i,
           use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
-        // {
-        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        //   type: 'asset/resource',
-        //   use: ['url-loader', 'file-loader']
-        // },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset',
+        },
         {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
